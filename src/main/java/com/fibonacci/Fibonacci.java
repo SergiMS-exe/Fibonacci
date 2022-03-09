7513362818fb7e890cab4077ac4a2a02a6d7e2d9
Sergio M <uo271563@uniovi.es>
2022-03-09 17:11:29 +0100

    Implementacion funcional

---
+++ src/main/java/com/fibonacci/Fibonacci.java
@@ -0,0 +1,18 @@
+package com.fibonacci;
+
+public class Fibonacci {
+
+    public int compute(int numero){
+        if (numero<0)
+            throw new RuntimeException("Number can't be negative");
+        else if (numero==1) {  // caso base
+            return 1;
+        }
+        else if (numero==0){  // caso base
+            return 0;
+        }
+        else{
+            return compute(numero-1) + compute(numero-2);
+        }
+    }
+}
