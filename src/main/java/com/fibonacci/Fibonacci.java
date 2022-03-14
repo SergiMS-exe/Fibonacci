8a2e1a67d5c9efb99a62616c6413d3aa4fe8195a
Sergio M <uo271563@uniovi.es>
2022-03-14 16:50:04 +0100

    entregable anotaciones y asertos

--- src/main/java/com/fibonacci/Fibonacci.java
+++ src/main/java/com/fibonacci/Fibonacci.java
@@ -2,17 +2,15 @@ package com.fibonacci;
 
 public class Fibonacci {
 
-    public int compute(int numero){
-        if (numero<0)
+    public int compute(int numero) {
+        if (numero < 0)
             throw new RuntimeException("Number can't be negative");
-        else if (numero==1) {  // caso base
+        else if (numero == 1) {  // caso base
             return 1;
-        }
-        else if (numero==0){  // caso base
+        } else if (numero == 0) {  // caso base
             return 0;
-        }
-        else{
-            return compute(numero-1) + compute(numero-2);
+        } else {
+            return compute(numero - 1) + compute(numero - 2);
         }
     }
 }
--- src/test/java/com/fibonacci/FibonacciTest.java
+++ src/test/java/com/fibonacci/FibonacciTest.java
@@ -1,39 +1,58 @@
 package com.fibonacci;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FibonacciTest {
 
     private Fibonacci fibonacci;
 
     @BeforeEach
-    void startUp(){
-        fibonacci=new Fibonacci();
+    void startUp() {
+        fibonacci = new Fibonacci();
     }
 
+    @Tag("invalid")
     @Test
-    void testInvalidNumber(){
-        assertThrows(RuntimeException.class,()->fibonacci.compute(-1));
+    void testInvalidNumber() {
+        assertThrows(RuntimeException.class, () -> fibonacci.compute(-1));
     }
 
+    @Tag("valid")
     @Test
-    void test0(){
+    void test0() {
         assertEquals(0, fibonacci.compute(0));
     }
 
+    @Tag("valid")
     @Test
-    void test1(){
+    void test1() {
         assertEquals(1, fibonacci.compute(1));
     }
 
+    @Tag("valid")
     @Test
-    void testValidNumbers(){
+    void testValidNumbers() {
         assertEquals(1, fibonacci.compute(2));
         assertEquals(2, fibonacci.compute(3));
         assertEquals(21, fibonacci.compute(8));
         assertEquals(55, fibonacci.compute(10));
+    }
+
+    @Tag("valid")
+    @Test
+    void testArray() {
+        Integer[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610};
+        List<Integer> actual = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            actual.add(fibonacci.compute(i));
+        }
+        assertArrayEquals(expected, actual.toArray());
     }
 }
